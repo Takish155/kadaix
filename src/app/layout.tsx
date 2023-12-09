286f605d93f53af9163b5575e1bf2c88b95fa03f
Takish Lim <rimutakeshi155@gmail.com>
2023-12-09 09:15:34 +0900

    added speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import AuthProvider from "./Provider";
 import QueryProvider from "./context/QueryProvider";
 import { UserInfoContextProvider } from "./context/UserInfoContext";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import NavBar from "./header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SpeedInsights />
         <AuthProvider>
           <QueryProvider>
             <UserInfoContextProvider>
